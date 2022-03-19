a4f76fd398c9e7a8a4cd8540c5616499043c83fe
AnandRochlani25 <anandrochlani25@gmail.com>
2022-03-19 13:23:51 +0530

    Proof of Concept

---
+++ javascript/ReturnType.js
@@ -0,0 +1,9 @@
+function checkPositive(number){
+    let flag=false;
+    if(number>0){
+    return flag;
+    }
+    return 0;
+}
+
+console.log(checkPositive(10));
---
+++ javascript/getPayment.js
@@ -0,0 +1,10 @@
+function getPayments(){
+    return 1;
+}
+
+function getLatestPayments(){
+    return "latest payments";
+}
+
+getPayments();
+getLatestPayments();
